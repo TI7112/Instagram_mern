913addcc722c30b50295ca4ff73de59f0a6754cb
ti7112 <ti7112@outlook.com>
2023-12-18 01:56:42 +0530

    mongo setup

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
+
 
 const connectMongoose = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/instagram')
+    mongoose.connect(`${process.env.MONGO_URI}`)
         .then((e) => { console.log('Connected to mongoDB') })
         .catch((e) => { console.log(e) })
 }
